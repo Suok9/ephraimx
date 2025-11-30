1a037acc4c45d2f8a98582cfd00a13ce735ce3a7
Suok9 <suoephraim@gmail.com>
2025-11-30 15:01:50 +0100

    Update recipes.js

--- js/api/recipes.js
+++ js/api/recipes.js
@@ -1,6 +1,6 @@
  export async function searchRecipes(query) {
-    const appId = "82e453da"; // Replace with your Edamam Recipe App ID
-    const appKey = "3bb5d1a3b992f408b9003effd74c9c22"; // Replace with your Edamam Recipe App Key
+    const appId = "82e453da"; 
+    const appKey = "3bb5d1a3b992f408b9003effd74c9c22"; 
     
     const url = `https://api.edamam.com/search?q=${encodeURIComponent(query)}&app_id=${appId}&app_key=${appKey}`;
     
